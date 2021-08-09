01bf8380436f1230213a4ce1835badd3c0463f19
Italo Ferreira <grd788@gmail.com>
2021-08-09 15:13:22 -0300

    Finalização do projeto

--- scripts.js
+++ scripts.js
@@ -15,11 +15,10 @@ for (const link of links){
   })
 }
 
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 function changeHeaderWhenScroll() {
-  
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
 
   if(this.window.scrollY >= navHeight){
     header.classList.add('scroll')
@@ -38,7 +37,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767:{
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
@@ -61,9 +66,9 @@ scrollReveal.reveal(
 )
 
 
+const backToTheTopButton = document.querySelector('.back-to-top')
 
 function  backToTheTop() {
-  const backToTheTopButton = document.querySelector('.back-to-top')
 
   if(this.window.scrollY >= 560){
     backToTheTopButton.classList.add('show')
@@ -72,7 +77,34 @@ function  backToTheTop() {
   }
 }
 
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuSection(){
+
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
+}
+
 window.addEventListener('scroll', function() {
   changeHeaderWhenScroll()
   backToTheTop()
+  activateMenuSection()
 })
